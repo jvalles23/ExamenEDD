d726aeceebd733320d934c7cfdaf5b944b68355c
jvalles23 <wua.valles23@gmail.com>
2022-03-10 17:16:34 +0100

    Segundo commit

---
+++ Ejercicio1.java
@@ -0,0 +1,14 @@
+package tarea4;
+
+public class Ejercicio1 {	
+	/**
+	 * Pre:---
+	 * Post: En este ejercicio me pide que muestre por pantalla 
+	 * 		"Los bucles son muy sencillos" siempre que i sea menor a 10.
+	 */
+		public static void main(String[] args) {						
+			for(int i = 0; i < 10; i++ ) {		//Iniciamos el bucle for metiendo las variables y el fin del bucle.
+				System.out.println( "Los bucles son muy sencillos");
+			}
+		}
+}
